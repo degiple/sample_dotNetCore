bfa89b6c6e89f4926969cf95b923d8aa675f8878
konosuke115 <algas115@gmail.com>
2020-04-06 23:09:35 +0900

    Replacing project

---
+++ sample-aspmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sample_aspmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
